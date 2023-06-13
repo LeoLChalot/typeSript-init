c6d7ecc5d89379a025fd5d3855150d59e6043658
leolchalot <larouchalotleo@gmail.com>
2023-06-14 00:05:21 +0200

    Initialisation de l'environnement typeScript

---
+++ dist/main.bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+const test = "123";
+console.log(test);
+
+/******/ })()
+;
+//# sourceMappingURL=main.bundle.js.map
